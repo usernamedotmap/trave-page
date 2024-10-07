29d10d364c09b320f87909eeadde3d19131f2b18
usernamedotmap <benasjoshuac@gmail.com>
2024-10-07 13:01:33 +0800

    maraming naiba

--- front-end/js.js
+++ front-end/js.js
@@ -30,13 +30,35 @@ setInterval(nextSlide, slideInterval);
 // changing color of the nav background
 
 const links = document.querySelectorAll("nav ul li a");
- 
+
 links.forEach(function (link) {
   link.onmouseover = function () {
-    document.querySelector(".header-container").style.background = "rgba(255, 255, 255, .17)"; /* #8c7f85, #4f494c */
+    document.querySelector(".header-container").style.background =
+      "rgba(255, 255, 255, .17)"; /* #8c7f85, #4f494c */
   };
 
   link.onmouseout = function () {
-    document.querySelector(".header-container").style.background = "transparent";
+    document.querySelector(".header-container").style.background =
+      "transparent";
   };
 });
+
+// pop up ng text the image ko
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const cards = document.querySelectorAll(".content-card");
+
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach(function (entry) {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("animate");
+//       } else {
+//         entry.target.classList.remove("animate");
+//       }
+//     });
+//   });
+
+//   cards.forEach(function (card) {
+//     observer.observe(card);
+//   });
+// });
